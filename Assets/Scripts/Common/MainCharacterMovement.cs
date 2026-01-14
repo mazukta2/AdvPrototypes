e99ac7525368760dbb73258fd461b6d40462d783
Daniil Shivanov <d.shivanov@chillgaming.net>
2026-01-14 18:54:41 +0100

    basic movement

--- Assets/Scripts/Common/MainCharacterMovement.cs
+++ Assets/Scripts/Common/MainCharacterMovement.cs
@@ -12,7 +12,7 @@ public class MainCharacterMove : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
