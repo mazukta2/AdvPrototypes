2bcc9e7425dc98e2791285df1be342afd5dc0fd0
Daniil Shivanov <d.shivanov@chillgaming.net>
2026-01-16 20:42:59 +0100

    tooltips

---
+++ Assets/Scripts/Common/Tooltip.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Common
+{
+    public class Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        public string Name;
+        [Multiline]public string Description;
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            TooltipWindow.Add(this);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            TooltipWindow.Remove(this);
+        }
+    }
+}
---
+++ Assets/Scripts/Common/TooltipWindow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+
+namespace Common
+{
+    public class TooltipWindow : SingletonMonoBehavior<TooltipWindow>
+    {
+        public TextMeshProUGUI Name;
+        public TextMeshProUGUI Description;
+
+        private List<Tooltip> _list = new List<Tooltip>();
+
+        public static void Add(Tooltip tooltip)
+        {
+            Instance._list.Add(tooltip);
+            Instance.UpdateText();
+        }
+        
+        public static void Remove(Tooltip tooltip)
+        {
+            Instance._list.Remove(tooltip);
+            Instance.UpdateText();
+        }
+
+        private  void UpdateText()
+        {
+            if (_list.Count == 0)
+            {
+                Name.text = "";
+                Description.text  = "";
+            }
+            else
+            {
+                var instance = _list.First();
+                Name.text  = instance.Name;
+                Description.text  = instance.Description;
+            }
+        }
+
+    }
+}
